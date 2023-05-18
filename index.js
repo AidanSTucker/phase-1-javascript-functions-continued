579416bafffeda447e56620c17c29f575d9eaa9e
elpatron <aidanseantucker@gmail.com>
2023-05-18 15:18:59 -0400

    made a very flary text wrapper

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(event = "roller-skate") {
+    return(`This Saturday, I want to ${event}!`);
+}
+
+
+
+function mondayWork(work = "go to the office") {
+    return(`This Monday, I will ${work}.`);
+}
+
+
+function wrapAdjective(input = "||") {
+   return function (flare = "special") {
+    return (`You are ${input}${flare}${input}!`);
+    }
+}
